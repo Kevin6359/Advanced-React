29b088a2d54be08efef33d4504c4f475880c209a
Kevin6359 <kevin.satti@webtrends-optimize.com>
2022-06-01 17:30:55 +0100

    styled components

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,5 +1,12 @@
+import NProgress from 'nprogress';
+import '../components/styles/nprogress.css';
+import Router from 'next/router';
 import PropTypes from 'prop-types';
 import Page from '../components/Page';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
