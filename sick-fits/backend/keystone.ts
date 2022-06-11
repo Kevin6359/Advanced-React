b9495dde7280ade46708142b38df6d845baf5201
Kevin6359 <kevin.satti@webtrends-optimize.com>
2022-06-11 13:47:53 +0100

    initialise users for backend

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
-
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import { User } from './schemas/User';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongobd://localhost/keystone-sick-fits-tutorial';
@@ -26,6 +26,7 @@ export default config({
 
   lists: createSchema({
     // Schema items go in here
+    User,
   }),
 
   ui: {
