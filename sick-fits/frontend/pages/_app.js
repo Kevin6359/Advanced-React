3040b53cef780b8526576b9e3c18b19edfe8a723
Kevin6359 <kevin.satti@webtrends-optimize.com>
2022-07-12 19:46:40 +0100

    boilerplate

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -2,21 +2,37 @@ import NProgress from 'nprogress';
 import '../components/styles/nprogress.css';
 import Router from 'next/router';
 import PropTypes from 'prop-types';
+import { ApolloProvider } from '@apollo/client';
 import Page from '../components/Page';
+import withData from '../lib/withData';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
 
 MyApp.propTypes = {
   pageProps: PropTypes.any,
   Component: PropTypes.any,
+  apollo: PropTypes.any,
 };
