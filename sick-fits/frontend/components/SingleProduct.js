425b2dbb9fb7bd1ad16412a0f68cbabfdcf1616e
Kevin6359 <kevin.satti@webtrends-optimize.com>
2022-08-15 20:41:12 +0100

    individual product pages and relevant components

---
+++ sick-fits/frontend/components/SingleProduct.js
@@ -0,0 +1,56 @@
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/client';
+import Head from 'next/head';
+import styled from 'styled-components';
+import DisplayError from './ErrorMessage';
+
+const ProductStyles = styled.div`
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  max-width: var(--maxWidth);
+  gap: 20px;
+  img {
+    width: 100%;
+  }
+`;
+
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    Product(where: { id: $id }) {
+      name
+      price
+      description
+      id
+      photo {
+        altText
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
+
+export default function SingleProduct({ id }) {
+  const { data, loading, error } = useQuery(SINGLE_ITEM_QUERY, {
+    variables: { id },
+  });
+  if (loading) return <p>Loading...</p>;
+  if (error) return <DisplayError error={error} />;
+  return (
+    <ProductStyles>
+      <Head>
+        <title>Sick Fits | {data.Product.name}</title>
+      </Head>
+      <img
+        src={data.Product.photo[0].image.publicUrlTransformed}
+        alt={data.Product.photo[0].altText}
+      />
+      <div className="details">
+        <h2>{data.Product.name}</h2>
+        <p>{data.Product.description}</p>
+      </div>
+    </ProductStyles>
+  );
+}
