f2bf2fe913c32a37269357518dc1c31b6bc715ad
Kevin6359 <kevin.satti@webtrends-optimize.com>
2023-06-23 08:52:59 +0100

    create stripe charge via frontend

--- sick-fits/frontend/components/Checkout.js
+++ sick-fits/frontend/components/Checkout.js
@@ -4,6 +4,8 @@ import styled from 'styled-components';
 import nProgress from 'nprogress';
 import {useState} from 'react';
 import SickButton from './styles/SickButton';
+import gql from 'graphql-tag';
+import {useMutation} from '@apollo/client';
 
 const CheckoutFormStyles = styled.form`
   box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
@@ -14,6 +16,20 @@ const CheckoutFormStyles = styled.form`
   grid-gap: 1rem;
 `;
 
+const CREATE_ORDER_MUTATION = gql`
+    mutation CREATE_ORDER_MUTATION ($token: String!){
+        checkout (token: $token){
+            id
+            charge
+            total
+            items {
+                id
+                name
+            }
+        }
+    } 
+`
+
 const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_TOKEN);
 
 function CheckoutForm() {
@@ -22,6 +38,9 @@ function CheckoutForm() {
     const [loading, setLoading] = useState(false);
     const stripe = useStripe();
     const elements = useElements();
+    const [checkout, {error: graphQLError}] = useMutation(
+        CREATE_ORDER_MUTATION
+    );
 
     async function handleSubmit(e){
         e.preventDefault();
@@ -33,11 +52,22 @@ function CheckoutForm() {
             card: elements.getElement(CardElement)
         });
 
-        // console.log(paymentMethod);
+        console.log(paymentMethod);
 
         if(error){
             setError(error);
+            nProgress.done();
+            return;
         }
+
+        const order = await checkout({
+            variables: {
+                token: paymentMethod.id
+            }
+        });
+
+        console.log('finished with the order');
+        console.log(order);
 
         setLoading(false);
         nProgress.done();
@@ -46,6 +76,7 @@ function CheckoutForm() {
   return (
     <CheckoutFormStyles onSubmit={handleSubmit}>
         {error && <p style={{ fontSize: 12}}>{error.message}</p>}
+        {graphQLError && <p style={{ fontSize: 12}}>{graphQLError.message}</p>}
         <CardElement />
         <SickButton type="submit">Check Out Now</SickButton>
     </CheckoutFormStyles>
